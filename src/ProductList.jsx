143c1eda0368ff8f34c3f82f6661c6966d3827ac
LilolambofGod <amanda.zheng1312@gmail.com>
2024-07-22 22:43:17 -0400

    updated product list display

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -267,15 +268,30 @@ const handlePlantsClick = (e) => {
             </div>
         </div>
         {!showCart? (
+        
         <div className="product-grid">
-
-
+         {plantsArray.map((category, index) => (
+           <div key={index}>
+           <h1><div>{category.category}</div></h1>
+           <div className="product-list">
+           {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+            <img className="product-image" src={plant.image} alt={plant.name} />
+            <div className="product-title">{plant.name}</div>
+            <div className="product-description">{plant.description}</div>
+            <div> ${plant.cost} </div>
+            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
         </div>
- ) :  (
+           ))}
+           </div>
+  </div>
+))}
+        </div>
+):(
     <CartItem onContinueShopping={handleContinueShopping}/>
 )}
-    </div>
-    );
+</div>
+);
 }
 
 export default ProductList;
